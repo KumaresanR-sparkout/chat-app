851d200927f39597ef438805eb13c943e4b46d04
KumaresanR-sparkout <kumaresan@sparkouttech.com>
2024-05-17 18:34:29 +0530

    filter:condition added

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -5,7 +5,8 @@ import * as response from '../utils/response.util'
 export const privateMessage = async (req, res) => {
     try {
         const array = [req.query.senderId, req.query.receiverId]
-        const message = await Message.find().populate({ path: 'chat_ref', select: { 'createdAt': 0, 'updatedAt': 0 }, populate: { path: 'users', select: { 'user_name': 1 } } })
+        const message = await Message.find()
+            .populate({ path: 'chat_ref', select: { 'createdAt': 0, 'updatedAt': 0 }, populate: { path: 'users', select: { 'user_name': 1 } } })
             .then(data => data.filter(data => {
                 console.log(data.sender_id)
                 return array.includes(data.sender_id.toString())
@@ -22,7 +23,8 @@ export const groupMessage = async (req, res) => {
     try {
         const message = await GroupMessage.find()
             .populate({ path: 'group_id', select: { "__v": 0 } })
-            .populate({ path: 'sender_id',select:{'user_name':1}, options: { strictPopulate: false } })
+            .populate({ path: 'sender_id', select: { 'user_name': 1 }, options: { strictPopulate: false } })
+            .then(data => data.filter(data => data.group_id._id == req.params.id))
 
         console.log(message)
         return response.sendSuccess(res, 200, "data fetched by the group based on users", message)
